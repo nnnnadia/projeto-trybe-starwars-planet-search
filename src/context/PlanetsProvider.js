fc4c1580a5c624690d2e886c0319f5089d83bcd0
nnnnadia <tristaonadia@gmail.com>
2022-07-11 19:08:16 -0300

    Configuração do contexto. Quando o provedor é montado é feita requisição à API.

---
+++ src/context/PlanetsProvider.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import PlanetsContext from './PlanetsContext';
+
+function PlanetsProvider({ children }) {
+  const [data, setData] = useState([]);
+  async function fetchPlanetsData() {
+    const ENDPOINT = 'https://swapi-trybe.herokuapp.com/api/planets/';
+    const fetchedPlanets = await fetch(ENDPOINT)
+      .then((response) => response.json())
+      .then((parsedData) => parsedData);
+    const resultsWithoutResidents = [];
+    fetchedPlanets.results
+      .forEach((planet) => {
+        delete planet.residents;
+        resultsWithoutResidents.push(planet);
+      });
+    setData(resultsWithoutResidents);
+  }
+  useEffect(() => fetchPlanetsData(), []);
+  const CONTEXT_VALUE = {
+    data,
+  };
+  return (
+    <PlanetsContext.Provider value={ CONTEXT_VALUE }>
+      { children }
+    </PlanetsContext.Provider>
+  );
+}
+
+PlanetsProvider.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.shape({}),
+    PropTypes.arrayOf(PropTypes.shape({})),
+  ]).isRequired,
+};
+
+export default PlanetsProvider;
